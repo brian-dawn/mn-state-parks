4bb19dd4353905afb43504fa855f5447c0028bed
Brian Dawn <brian.t.dawn@gmail.com>
2022-09-13 15:50:23 -0500

    Apply formatting.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use anyhow::Result;
 
 pub mod api;
 
-
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let resp_place = api::fetch_place().await?;
     println!("{:#?}", resp_place);
     // let resp_parks = api::fetch_parks().await?;
